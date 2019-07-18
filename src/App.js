2dfc8765b8d0a567d6aa2b36cbdb7263fbdd86e4
Anadeius <Anadeius@users.noreply.github.com>
2019-07-17 22:17:20 -0400

    Request Component
    Breaking down App into Container components

--- src/App.js
+++ src/App.js
@@ -138,18 +138,42 @@ function App() {
             <span className="requestSubTitleMinimal">  Descent</span>
           </h3>
           <hr className="divider" />
+          <RequestMinimal role='Healer' twitchName='anadeius' characterName='Anadeius Etherius' timestamp='July 17, 2019 - 4:15am' />
+          <RequestMinimal role='Tank' twitchName='mtqcapture' characterName='Mizzteq Aran' timestamp='July 18, 2019 - 12:15pm'  />
+          <RequestMinimal role='Damage' twitchName='solu_9' characterName="Harlequin D'ranged" timestamp='July 21, 2019 - 3:15pm' />
+          <RequestMinimal role='Tank' twitchName='amazeringone' characterName='Amazering One' timestamp='July 30, 2019 - 6:15pm' />
+          <RequestMinimal role='Healer' twitchName='anadeius' characterName='Anadeius Etherius' timestamp='July 17, 2019 - 4:15am' />
+          <RequestMinimal role='Tank' twitchName='mtqcapture' characterName='Mizzteq Aran' timestamp='July 18, 2019 - 12:15pm'  />
+          <RequestMinimal role='Damage' twitchName='solu_9' characterName="Harlequin D'ranged" timestamp='July 21, 2019 - 3:15pm' />
+          <RequestMinimal role='Tank' twitchName='amazeringone' characterName='Amazering One' timestamp='July 30, 2019 - 6:15pm' />
         </section>
         <section>
           <h3 className="requestTitleMinimal">Eden's Gate: 
             <span className="requestSubTitleMinimal">  Inundation</span>
           </h3>
           <hr className="divider" />
+          <RequestMinimal role='Healer' twitchName='anadeius' characterName='Anadeius Etherius' timestamp='July 17, 2019 - 4:15am' />
+          <RequestMinimal role='Tank' twitchName='mtqcapture' characterName='Mizzteq Aran' timestamp='July 18, 2019 - 12:15pm'  />
+          <RequestMinimal role='Damage' twitchName='solu_9' characterName="Harlequin D'ranged" timestamp='July 21, 2019 - 3:15pm' />
+          <RequestMinimal role='Tank' twitchName='amazeringone' characterName='Amazering One' timestamp='July 30, 2019 - 6:15pm' />
+          <RequestMinimal role='Healer' twitchName='anadeius' characterName='Anadeius Etherius' timestamp='July 17, 2019 - 4:15am' />
+          <RequestMinimal role='Tank' twitchName='mtqcapture' characterName='Mizzteq Aran' timestamp='July 18, 2019 - 12:15pm'  />
+          <RequestMinimal role='Damage' twitchName='solu_9' characterName="Harlequin D'ranged" timestamp='July 21, 2019 - 3:15pm' />
+          <RequestMinimal role='Tank' twitchName='amazeringone' characterName='Amazering One' timestamp='July 30, 2019 - 6:15pm' />
         </section>
         <section>
           <h3 className="requestTitleMinimal">Eden's Gate: 
             <span className="requestSubTitleMinimal">  Sepulture</span>
           </h3>
           <hr className="divider" />
+          <RequestMinimal role='Healer' twitchName='anadeius' characterName='Anadeius Etherius' timestamp='July 17, 2019 - 4:15am' />
+          <RequestMinimal role='Tank' twitchName='mtqcapture' characterName='Mizzteq Aran' timestamp='July 18, 2019 - 12:15pm'  />
+          <RequestMinimal role='Damage' twitchName='solu_9' characterName="Harlequin D'ranged" timestamp='July 21, 2019 - 3:15pm' />
+          <RequestMinimal role='Tank' twitchName='amazeringone' characterName='Amazering One' timestamp='July 30, 2019 - 6:15pm' />
+          <RequestMinimal role='Healer' twitchName='anadeius' characterName='Anadeius Etherius' timestamp='July 17, 2019 - 4:15am' />
+          <RequestMinimal role='Tank' twitchName='mtqcapture' characterName='Mizzteq Aran' timestamp='July 18, 2019 - 12:15pm'  />
+          <RequestMinimal role='Damage' twitchName='solu_9' characterName="Harlequin D'ranged" timestamp='July 21, 2019 - 3:15pm' />
+          <RequestMinimal role='Tank' twitchName='amazeringone' characterName='Amazering One' timestamp='July 30, 2019 - 6:15pm' />
         </section>
       </section>
 
---
+++ src/components/RequestContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class RequestContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            aetherRequests = {},
+            primalRequests = {}
+        };
+    }
+    render() {
+        return (
+            <div></div>
+        )
+    };
+}
+
+export default RequestContainer;
